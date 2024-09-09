0bb84d7a31513bc29daf3de3cc859b2bfd9f1da8
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2024-09-09 17:51:06 +0800

    navbar initial updates

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+
+import Navigation from "./navigation";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navigation />
+      </header>
+      <body className={"App-body"}>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit 去  <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +23,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </body>
     </div>
   );
 }
