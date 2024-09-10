b3f77573e9c0357059e896c45c5c6ff3d44c3422
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2024-09-10 10:08:47 +0800

    Move navigation bar into components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 
-import Navigation from "./navigation";
+import Navigation from "./components/navigation";
 
 function App() {
   return (
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -2,14 +2,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
-import logo from "./logo.svg";
+import logo from "../logo.svg";
 
 function Navigation() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary" fixed={"top"}>
       <Container>
         <Navbar.Brand href="#home">
-          <img src={require("./img/logo.png")} alt="ShanghaiBaoXuan"/>
+          <img src={require("../img/logo.png")} alt="ShanghaiBaoXuan"/>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
