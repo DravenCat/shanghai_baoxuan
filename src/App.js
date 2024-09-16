c912cf4166ae59e6e3b217a755c869d3fb955117
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2024-09-17 00:46:45 +0800

    Add constum size

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css'
 import {BrowserRouter as Router, Routes, Route, Navigate} from "react-router-dom";
+import { useSize } from "ahooks";
+import { useMemo, useRef } from "react";
 
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
@@ -12,13 +14,27 @@ import Navigation from "./components/Navigation";
 
 function App() {
 
+  // 获取需要自适应缩放的 DOM 元素引用
+  const ref = useRef(null);
+
+  // 获取 document.body 的尺寸
+  const size = useSize(document.body);
+
+  // 计算合适的缩放比例
+  const zoom = useMemo(() => {
+    if (!size) return 1;
+    return size.width / 1680;
+  }, [size]);
 
   return (
-    <div className="App">
+    <div ref={ref} style={{ width: "100%", height: "100%", zoom }}>
+      <div className="App">
       <Router>
         <header>
           <Navigation />
         </header>
+        <div className={'App-screen'}>
+        <div  >
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile/*' element={<Profile />} />
@@ -26,11 +42,15 @@ function App() {
           <Route path='/contact' element={<Contact />} />
           <Route path="*" element={<Navigate to="/" replace={true}/>} />
         </Routes>
+        </div>
+        </div>
       </Router>
       <footer className="App-footer">
         <Footer />
       </footer>
     </div>
+  </div>
+
 
   );
 }
