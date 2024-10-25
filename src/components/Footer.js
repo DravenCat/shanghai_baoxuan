61ec41d7d427f90dd990e26b6a691346399e4e41
Dezhi Ren <dezhi.ren@mail.utoronto.ca>
2024-10-25 11:00:17 +0800

    Add ICP code

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,6 @@
+import "./Footer.css"
+
+
 function Footer() {
 
   const getLanguage = () => {
@@ -15,14 +18,17 @@ function Footer() {
   return(
     <div className="App-footer">
       {isChinese?
-        <div className='text-center p-3'>
+        <div className='text-center p-sm-2 copyright'>
           © 2024 上海宝宣自动化控制系统有限公司版权所有
         </div>
         :
-        <div className='text-center p-3'>
+        <div className='text-center p-sm-2 copyright'>
         Copyright © 2024 Shanghai Baoxuan Automation Control System Co. LTD. Built with React
         </div>
       }
+      <div className='text-center pb-2'>
+         <a href={"https://beian.miit.gov.cn/#/Integrated/index"} className="ICP-code">沪ICP备2024099341号 </a>
+      </div>
     </div>
   );
 }
